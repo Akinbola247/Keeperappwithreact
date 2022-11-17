6aeae4e6f8b400df698fae7d50c62e28750b15f3
akinbola247 <akinbolakehinde247@gmail.com>
2022-11-17 05:22:40 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import StarIcon from "@mui/icons-material/Star";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {" "}
+        <StarIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
